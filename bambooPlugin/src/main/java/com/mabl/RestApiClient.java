21c7edfb79fe74e924eb0f0a7b4986de7e23595b
Mike DeCosta <mike_decosta@yahoo.com>
2018-05-23 12:27:45 -0400

    better validation, polling of execution results

--- bambooPlugin/src/main/java/com/mabl/RestApiClient.java
+++ bambooPlugin/src/main/java/com/mabl/RestApiClient.java
@@ -14,6 +14,7 @@ import java.util.Base64;
 import java.util.concurrent.TimeUnit;
 
 import com.mabl.domain.ExecutionResult;
+import com.mabl.domain.GetApiKeyResult;
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
@@ -32,33 +33,30 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicHeader;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+import static com.mabl.MablConstants.MABL_REST_API_BASE_URL;
+import static com.mabl.MablConstants.REQUEST_TIMEOUT_MILLISECONDS;
 import static org.apache.commons.httpclient.HttpStatus.SC_CREATED;
-import static org.apache.commons.httpclient.HttpStatus.SC_NOT_FOUND;
 import static org.apache.commons.httpclient.HttpStatus.SC_OK;
 
-public class RestApiClient {
+public class RestApiClient implements AutoCloseable {
     private final String restApiKey;
     private final CloseableHttpClient httpClient;
+    private final Logger.Log log = Logger.getInstance(this.getClass());
 
     private final ObjectMapper objectMapper = new ObjectMapper();
-
     {
         objectMapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
         objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
     }
-    private final Logger.Log log = Logger.getInstance(this.getClass());
 
-    static final String DEPLOYMENT_TRIGGER_ENDPOINT = "/events/deployment";
-    static final String DEPLOYMENT_RESULT_ENDPOINT_TEMPLATE = "/execution/result/event/%s";
-    static final String REST_API_USERNAME_PLACEHOLDER = "key";
-    private static final String restApiBaseUrl = "https://api.mabl.com";
+    private static final String DEPLOYMENT_TRIGGER_ENDPOINT = "/events/deployment";
+    private static final String DEPLOYMENT_RESULT_ENDPOINT_TEMPLATE = "/execution/result/event/%s";
+    private static final String GET_ORGANIZATION_ENDPOINT_TEMPLATE = "/apiKeys/%s";
+    private static final String REST_API_USERNAME_PLACEHOLDER = "key";
     private static final Header JSON_TYPE_HEADER = new BasicHeader("Content-Type", "application/json");
-    private static final String PLUGIN_VERSION_UNKNOWN = "unknown";
-    private static final String PLUGIN_USER_AGENT = "mabl-bamboo-plugin/" + PLUGIN_VERSION_UNKNOWN;
-    private static final int REQUEST_TIMEOUT_MILLISECONDS = 60000;
 
     public RestApiClient(String restApiKey) {
         this.restApiKey = restApiKey;
@@ -66,7 +64,7 @@ public class RestApiClient {
     }
 
     public CreateDeploymentResult createDeploymentEvent(final String environmentId, final String applicationId) {
-        final HttpPost request = buildPostRequest(restApiBaseUrl + DEPLOYMENT_TRIGGER_ENDPOINT);
+        final HttpPost request = new HttpPost(MABL_REST_API_BASE_URL + DEPLOYMENT_TRIGGER_ENDPOINT);
         request.setEntity(getCreateDeplotmentPayloadEntity(environmentId, applicationId));
         request.addHeader(getBasicAuthHeader(restApiKey));
         request.addHeader(JSON_TYPE_HEADER);
@@ -74,29 +72,13 @@ public class RestApiClient {
     }
 
     public ExecutionResult getExecutionResults(final String eventId) {
-        final String url = restApiBaseUrl + String.format(DEPLOYMENT_RESULT_ENDPOINT_TEMPLATE, eventId);
+        final String url = MABL_REST_API_BASE_URL + String.format(DEPLOYMENT_RESULT_ENDPOINT_TEMPLATE, eventId);
         return parseApiResult(getResponse(buildGetRequest(url)), ExecutionResult.class);
     }
 
-    private HttpGet buildGetRequest(final String url) {
-        try {
-            final HttpGet request = new HttpGet(url);
-            request.addHeader(getBasicAuthHeader(restApiKey));
-            return request;
-        } catch (IllegalArgumentException e) {
-            throw new RuntimeException(String.format("Unexpected status from mabl trying to build API url: %s", url));
-        }
-    }
-
-    private AbstractHttpEntity getCreateDeplotmentPayloadEntity(String environmentId, String applicationId) {
-        try {
-            final String jsonPayload = objectMapper.writeValueAsString(new CreateDeploymentPayload(environmentId, applicationId));
-            return new ByteArrayEntity(jsonPayload.getBytes("UTF-8"));
-
-        } catch (IOException e) {
-            log.error(String.format("Unable to create payloadEntity"));
-            throw new RuntimeException(e.getMessage(), e);
-        }
+    public GetApiKeyResult getApiKeyResult(String formApiKey) {
+        final String url = MABL_REST_API_BASE_URL + String.format(GET_ORGANIZATION_ENDPOINT_TEMPLATE, formApiKey);
+        return parseApiResult(getResponse(buildGetRequest(url)), GetApiKeyResult.class);
     }
 
     private CloseableHttpClient getHttpClient(String restApiKey) {
@@ -104,34 +86,48 @@ public class RestApiClient {
                 .setRedirectStrategy(new DefaultRedirectStrategy())
                 .setServiceUnavailableRetryStrategy(getRetryHandler())
                 .setDefaultCredentialsProvider(getApiCredentialsProvider(restApiKey))
-                .setUserAgent(PLUGIN_USER_AGENT) // track calls @ API level
-                .setConnectionTimeToLive(30, TimeUnit.SECONDS) // use keep alive in SSL API connections
+                .setUserAgent(MablConstants.PLUGIN_USER_AGENT)
+                .setConnectionTimeToLive(MablConstants.CONNECTION_SECONDS_TO_LIVE, TimeUnit.SECONDS)
                 .setDefaultRequestConfig(getDefaultRequestConfig())
                 .build();
     }
 
+    private MablRestApiClientRetryHandler getRetryHandler() {
+        return new MablRestApiClientRetryHandler(
+                MablConstants.RETRY_HANDLER_MAX_RETRIES,
+                MablConstants.RETRY_HANDLER_RETRY_INTERVAL
+        );
+    }
+
+    private CredentialsProvider getApiCredentialsProvider(final String restApiKey) {
+        final CredentialsProvider provider = new BasicCredentialsProvider();
+        final UsernamePasswordCredentials creds =
+                new UsernamePasswordCredentials(REST_API_USERNAME_PLACEHOLDER, restApiKey);
+
+        provider.setCredentials(AuthScope.ANY, creds);
+        return provider;
+    }
+
     private RequestConfig getDefaultRequestConfig() {
-        // TODO we should retry connection timeouts
         return RequestConfig.custom()
                 .setConnectTimeout(REQUEST_TIMEOUT_MILLISECONDS)
                 .setConnectionRequestTimeout(REQUEST_TIMEOUT_MILLISECONDS)
                 .setSocketTimeout(REQUEST_TIMEOUT_MILLISECONDS)
                 .build();
     }
-    private MablRestApiClientRetryHandler getRetryHandler() {
-        return new MablRestApiClientRetryHandler(5, 6000L);
-    }
 
-    private CredentialsProvider getApiCredentialsProvider(
-            final String restApiKey
-    ) {
-        final CredentialsProvider provider = new BasicCredentialsProvider();
-        final UsernamePasswordCredentials creds =
-                new UsernamePasswordCredentials(REST_API_USERNAME_PLACEHOLDER, restApiKey);
+    private AbstractHttpEntity getCreateDeplotmentPayloadEntity(String environmentId, String applicationId) {
+        try {
+            final String jsonPayload = objectMapper.writeValueAsString(
+                    new CreateDeploymentPayload(environmentId, applicationId)
+            );
 
-        provider.setCredentials(AuthScope.ANY, creds);
+            return new ByteArrayEntity(jsonPayload.getBytes("UTF-8"));
 
-        return provider;
+        } catch (IOException e) {
+            log.error(String.format("Unable to create payloadEntity. Reason: '%s'", e.getMessage()));
+            throw new RuntimeException(e.getMessage(), e);
+        }
     }
 
     private Header getBasicAuthHeader(final String restApiKey) {
@@ -139,30 +135,34 @@ public class RestApiClient {
         return new BasicHeader("Authorization", "Basic " + base64EncodeUtf8(toEncode));
     }
 
-    private String base64EncodeUtf8(String s) {
+    private String base64EncodeUtf8(String toBeEncoded) {
         try {
-            return Base64.getEncoder().encodeToString(s.getBytes("UTF-8"));
+            return Base64.getEncoder().encodeToString(toBeEncoded.getBytes("UTF-8"));
         } catch (UnsupportedEncodingException e) {
-            log.error(String.format("Unable to base64 encode using UTF-8 the string '%s'", s));
+            log.error(String.format(
+                    "Unable to base64 encode using UTF-8 the string '%s'. Reason:'%s'",
+                    toBeEncoded,
+                    e.getMessage()
+            ));
             throw new RuntimeException(e.getMessage(), e);
         }
     }
 
-    private HttpPost buildPostRequest(String url) {
-        try {
-            final HttpPost request = new HttpPost(url);
-            return request;
-        } catch (IllegalArgumentException e) {
-            log.error(String.format("Unexpected status from mabl trying to build API url: %s", url));
-            throw new RuntimeException(e.getMessage(), e);
-        }
+    private HttpGet buildGetRequest(final String url) {
+        final HttpGet request = new HttpGet(url);
+        request.addHeader(getBasicAuthHeader(restApiKey));
+        return request;
     }
 
     private HttpResponse getResponse(HttpUriRequest request) {
         try {
             return httpClient.execute(request);
         } catch (IOException e) {
-            log.error(String.format("Encountered exception trying to reach '%s' Reason: %s", request.getURI(), e.getMessage()));
+            log.error(String.format(
+                    "Encountered exception trying to reach '%s'. Reason: '%s'",
+                    request.getURI(),
+                    e.getMessage()
+            ));
             throw new RuntimeException(e.getMessage(), e);
         }
     }
@@ -170,14 +170,16 @@ public class RestApiClient {
     private <ApiResult> ApiResult parseApiResult(final HttpResponse response, Class<ApiResult> resultClass) {
         final int statusCode = response.getStatusLine().getStatusCode();
 
+        String message = "";
         switch (statusCode) {
             case SC_OK: // fall through case
             case SC_CREATED:
                 return mapObject(response, resultClass);
-            case SC_NOT_FOUND:
-                return null;
             default:
-                String message = String.format("Unexpected status from parse Api result during fetch %d%n", statusCode);
+                message = String.format(
+                        "Unexpected status returned from parse Api result during fetch %d%n",
+                        statusCode
+                );
                 log.error(message);
                 throw new RuntimeException(message);
         }
@@ -187,7 +189,11 @@ public class RestApiClient {
         try {
             return resultClass.cast(objectMapper.readerFor(resultClass).readValue(response.getEntity().getContent()));
         } catch (IOException e) {
-            log.error(String.format("Encountered exception trying to decode '%s' Reason: %s", resultClass.getName(), e.getMessage()));
+            log.error(String.format(
+                    "Encountered exception trying to decode '%s'. Reason: '%s'",
+                    resultClass.getName(),
+                    e.getMessage()
+            ));
             throw new RuntimeException(e.getMessage(), e);
         }
     }
@@ -197,9 +203,13 @@ public class RestApiClient {
             try {
                 httpClient.close();
             } catch (IOException e) {
-                // TODO cleaner exception handling
-                e.printStackTrace();
+                log.error(String.format(
+                        "Encountered exception trying to close httpClient. Reason: '%s'",
+                        e.getMessage()
+                ));
+                throw new RuntimeException(e.getMessage(), e);
             }
         }
     }
+
 }
