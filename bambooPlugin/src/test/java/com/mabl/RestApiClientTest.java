f63eb1081d8174324d0820ddbfa348a8c302c8f3
Mike DeCosta <mike_decosta@yahoo.com>
2018-05-23 15:47:50 -0400

    just commiting this before some changes

--- bambooPlugin/src/test/java/com/mabl/RestApiClientTest.java
+++ bambooPlugin/src/test/java/com/mabl/RestApiClientTest.java
@@ -1,21 +1,53 @@
 package com.mabl;
-//
-//import com.github.tomakehurst.wiremock.client.MappingBuilder;
-//import com.mabl.domain.CreateDeploymentResult;
+
+//import com.github.tomakehurst.wiremock.WireMockServer;
+//import com.github.tomakehurst.wiremock.client.WireMock;
+//import com.github.tomakehurst.wiremock.junit.WireMockRule;
 //import com.github.tomakehurst.wiremock.matching.EqualToPattern;
+//import com.mabl.domain.GetApiKeyResult;
+//import org.junit.Rule;
 //import org.junit.Test;
 //
-//import java.io.IOException;
-//import java.util.HashMap;
-//import java.util.Map;
 //
+//import com.github.tomakehurst.wiremock.client.MappingBuilder;
 //import static org.junit.Assert.assertEquals;
-//import static com.github.tomakehurst.wiremock.client.WireMock.post;
+//import static com.github.tomakehurst.wiremock.client.WireMock.get;
 //import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-//import static com.github.tomakehurst.wiremock.client.WireMock.created;
+//import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+//import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+//import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 //import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+
+public class RestApiClientTest {
+
+//    @Test
+//    public void getApiKeyResultTest() {
+//        WireMockServer wireMockServer = new WireMockServer(8084);
+//        WireMock.configureFor("localhost", 8084);
+//        wireMockServer.start();
 //
-public class RestApiClientTest extends AbstractWiremockTest {
+//        final String fakeRestApiKey = "fakeApiKeyValue";
+//        RestApiClient restApiClient = new RestApiClient(fakeRestApiKey);
+//
+//        final MappingBuilder mappingBuilder = get(urlPathEqualTo(restApiClient.GET_ORGANIZATION_ENDPOINT_TEMPLATE))
+//                .willReturn(ok()
+//                        .withHeader("Content-Type", "application/json")
+//                        .withBody(MablTestConstants.APIKEY_RESULT_JSON));
+//
+//        mappingBuilder.withBasicAuth(restApiClient.REST_API_USERNAME_PLACEHOLDER, fakeRestApiKey);
+//        mappingBuilder.withHeader("user-agent", new EqualToPattern(MablConstants.PLUGIN_USER_AGENT));
+//
+//        stubFor(get(urlEqualTo("/whatever"))
+//        .willReturn(aResponse()
+//                .withStatus(200)
+//                .withHeader("Content-Type", "application/json")
+//                .withHeader("Cache-Control", "no-cache")));
+//
+//        //stubFor(mappingBuilder);
+//
+//        GetApiKeyResult getApiKeyResult = restApiClient.getApiKeyResult(fakeRestApiKey);
+//        assertEquals("K8NWhtPqOyFnyvJTvCP0uw-w", getApiKeyResult.organization_id);
+//    }
 //
 //    private static final String EXPECTED_DEPLOYMENT_EVENT_ID = "d1To4-GYeZ4nl-4Ag1JyQg-v";
 //    private static final String EXPECTED_ORGANIZATION_ID = "K8NWhtPqOyFnyvJTvCP0uw-w";
