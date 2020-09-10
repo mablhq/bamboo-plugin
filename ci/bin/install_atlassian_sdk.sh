b11376ccfe05c6cfe6ab7040ef12286e9aaa8da5
Joseph Lust <lifeoflust@gmail.com>
2020-09-10 09:19:09 -0400

    GitHub Actions

---
+++ ci/bin/install_atlassian_sdk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Installing Atlassian SDK"
+sudo sh -c 'echo "deb https://packages.atlassian.com/debian/atlassian-sdk-deb/ stable contrib" >>/etc/apt/sources.list'
+wget https://packages.atlassian.com/api/gpg/key/public
+sudo apt-key add public
+sudo apt-get update
+sudo apt-get install atlassian-plugin-sdk
+
+echo "Atlassian SDK instalation complete"
